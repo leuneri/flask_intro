370d4a98eb9b725442a958ff1f8b081f436a673a
leung147 <ericleung147258369@hotmail.com>
2022-11-29 14:10:20 -0800

    error with json

--- test.py
+++ test.py
@@ -2,14 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 00, "name": "Vid0", "views": 20000},
-        {"likes": 10, "name": "Vid1", "views": 21000},
-        {"likes": 20, "name": "Vid2", "views": 22000}]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+response = requests.patch(BASE + 'video/2', {'views':99, 'likes':101})
+data = response.json()
+print(data)
 
-input()
-response = requests.get(BASE + "video/2")
-print(response.json())
+# data = [{"likes": 00, "name": "Vid0", "views": 20000},
+#         {"likes": 10, "name": "Vid1", "views": 21000},
+#         {"likes": 20, "name": "Vid2", "views": 22000}]
+
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
+
+# input()
+# response = requests.get(BASE + "video/2")
+# print(response.json())
