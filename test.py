04d953f92af0ea3cbf87420de5865a240726906e
leung147 <ericleung147258369@hotmail.com>
2022-11-29 14:01:11 -0800

    Use of SQLite

--- test.py
+++ test.py
@@ -2,5 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes":10})
+data = [{"likes": 00, "name": "Vid0", "views": 20000},
+        {"likes": 10, "name": "Vid1", "views": 21000},
+        {"likes": 20, "name": "Vid2", "views": 22000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
